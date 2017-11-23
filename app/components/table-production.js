946602a3f22e57399db4dc970a8ef3df23466210
Bruno Campos <bcampos@hugeinc.com>
2017-11-23 16:02:14 -0200

    Adding Reusable Components

---
+++ app/components/table-production.js
@@ -0,0 +1,12 @@
+import Component from '@ember/component';
+
+export default Component.extend({
+    tagName: 'table',
+    classNames: ['table'],
+    boards: Ember.computed.mapBy('model', 'boards'),
+    total_boards: Ember.computed.sum('boards'),
+    max_boards: Ember.computed.max('boards'),
+    boardfeets: Ember.computed.mapBy('model', 'boardfeet'),
+    total_boardfeet: Ember.computed.sum('boardfeets'),
+    max_boardfeet: Ember.computed.max('boardfeets')
+});
--- app/controllers/production/index.js
+++ app/controllers/production/index.js
@@ -6,11 +6,10 @@ export default Controller.extend({
     //         return prev + parseFloat(row.get('boards'));
     //     }, 0);
     // }),
-    boards: Ember.computed.mapBy('model', 'boards'),
-    total_boards: Ember.computed.sum('boards'),
-    max_boards: Ember.computed.max('boards'),
-
-    boardfeets: Ember.computed.mapBy('model', 'boardfeet'),
-    total_boardfeet: Ember.computed.sum('boardfeets'),
-    max_boardfeet: Ember.computed.max('boardfeets')
+    // boards: Ember.computed.mapBy('model', 'boards'),
+    // total_boards: Ember.computed.sum('boards'),
+    // max_boards: Ember.computed.max('boards'),
+    // boardfeets: Ember.computed.mapBy('model', 'boardfeet'),
+    // total_boardfeet: Ember.computed.sum('boardfeets'),
+    // max_boardfeet: Ember.computed.max('boardfeets')
 });
